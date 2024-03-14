fbe4a970b31150029c4982f66f0db7fcd559568c
tmarr1426 <tmarr1426@gmail.com>
2024-03-13 19:08:23 -0500

    Challenge complete

---
+++ react_auth/src/components/Product/Product.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// REACT CHALLENGE
+// ------------------------------------------
+// Store the results from the fetch in state
+// Map over the results, and return a child component called Product
+// This component will take an obj from the results as a prop.
+// Within the Product component, display the product emoji, name, price, and tags
+//? BONUS*
+// Setup an input field for max price, set state accordingly.
+// Setup 3 checkbox input fields, one for electronics, one for fruit, and one for snack
+//    - Using the state variables store the results from each input field when checked/unchecked into the 'tags' state variable.
+//    - The values which are stored in state, should be used in the fetch URL as dynamic query params
+
+const Product = ({ product }) => {
+  console.log(product);
+  return (
+    <div className="card">
+      <h1 className="card-product-emoji">{product.emoji}</h1>
+      <h3 className="card-product-name">{product.name}</h3>
+      <p className="card-product-price">{product.price}</p>
+      {product.tags.map(i => (
+        <li className="card-product-tag">{i.toUpperCase()}</li>
+      ))}
+    </div>
+  );
+};
+
+export default Product;
--- react_auth/src/components/Products/Products.jsx
+++ react_auth/src/components/Products/Products.jsx
@@ -1,33 +1,100 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+import Product from "../Product/Product";
+
 const Products = () => {
   const [results, setResults] = useState([]);
-  const [maxPrice, setMaxPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState(799);
   const [tags, setTags] = useState([]);
 
   useEffect(() => {
     const getProducts = async () => {
       try {
         const response = await fetch(
-          `http://localhost:8081/product?min=0&max=1000&tags=electronics`, {
+          `http://localhost:8081/product?min=0&${maxPrice}&tags=${tags}`,
+          {
             headers: {
-              Authorization: `Bearer ${localStorage.getItem("MyToken")}`
-            }
+              Authorization: `Bearer ${localStorage.getItem("MyToken")}`,
+            },
           }
         );
         const json = await response.json();
 
-        console.log(json);
+        console.log(json.Results);
+        setResults(json.Results);
       } catch (err) {
         console.log(err);
       }
     };
 
     getProducts();
-  }, []);
+  }, [maxPrice, tags]);
 
-  return <div>Products</div>;
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await fetch(
+  //       `http://localhost:8081/product?min=0&max=${maxPrice}&tags=electronics`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${localStorage.getItem("MyToken")}`,
+  //         },
+  //       }
+  //     );
+  //     const json = await response.json();
+  //     setResults(json.Results);
+  //     console.log(maxPrice, tags);
+  //   } catch (err) {
+  //     console.log("Error fetching products:", err);
+  //   }
+  // };
+
+  const handleTag = (e) => {
+    let tagName = e.target.name;
+    if (tags.includes(tagName)) {
+      setTags((previousTags) => previousTags.filter((i) => i !== tagName));
+    } else {
+      setTags((previousTags) => [...previousTags, tagName]);
+    }
+  };
+
+  return (
+    <div>
+      <input type="number" onChange={(e) => setMaxPrice(e.target.value)} />
+      <h1>Products</h1>
+      {tags.toString()}
+      <div>
+        <input
+          type="checkbox"
+          id="electronics"
+          name="electronics"
+          value="electronics"
+          onChange={handleTag}
+        />
+        <label htmlFor="electronics">Electronics</label>
+        <input
+          type="checkbox"
+          id="fruit"
+          name="fruit"
+          value="fruit"
+          onChange={handleTag}
+        />
+        <label htmlFor="fruit">Fruit</label>
+        <input
+          type="checkbox"
+          id="snack"
+          name="snack"
+          value="snack"
+          onChange={handleTag}
+        />
+        <label htmlFor="snack">Snack</label>
+      </div>
+      {results.map((obj) => (
+        <Product key={obj._id} product={obj} />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
